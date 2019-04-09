a5da4626645fd95aac63291b7aea020d48596963
Derek Hu [DATACOM] <derekh@datacom.co.nz>
2019-04-09 12:01:27 +1200

    Transpose theta value

--- python/linear regression/cost_function.py
+++ python/linear regression/cost_function.py
@@ -7,5 +7,6 @@ Created on Mon Apr  8 17:00:14 2019
 import numpy as np
 
 def computeCost(X, y, theta):
-    inner = np.power(((X * theta.T) - y), 2)
+    thetaT = theta.T
+    inner = np.power(((X * thetaT) - y), 2)
     return np.sum(inner) / (2 * len(X))
